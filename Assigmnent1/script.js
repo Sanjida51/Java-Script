38f7076a99bfff01768d26303077a983d787de0b
Sanjida Akter <137642044+Sanjida51@users.noreply.github.com>
2024-06-29 19:37:07 +0600

    Add files via upload

---
+++ Assigmnent1/script.js
@@ -0,0 +1,50 @@
+
+ //console.log('Hello, World');
+ //alert("JavaScript is connected!");
+ //alert("Welcome to JavaScript");
+ //document.write("Hello, DOM!");
+/*let myVariable;
+myVariable = 10;
+myVariable = 20;
+console.log(myVariable);*/
+/*let x = 10;
+if (x > 5) {
+    console.log("x is greater than 5");
+}*/
+// Single-line comment
+/*let a = 5;
+let b = 10;
+/* Multi-line comment
+   explaining the addition */
+/*let c = a + b;
+console.log(c);*/
+
+/*let name = "John Doe";
+let age = 25;
+let isStudent = true;
+
+console.log(name);
+console.log(age);
+console.log(isStudent);*/
+
+let value = 10;
+
+value += 5; // value = value + 5
+console.log(value);
+
+value -= 3; // value = value - 3
+console.log(value);
+
+value *= 2; // value = value * 2
+console.log(value);
+
+value /= 4; // value = value / 4
+console.log(value);
+
+value %= 3; // value = value % 3
+console.log(value);
+
+
+
+
+ 
